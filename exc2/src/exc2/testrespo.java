a2723420a02acf7a5d1ea6d7d2f07eb0b69661e0
Tiran1990 <t.hesawi1990@gmail.com>
2021-03-21 11:02:24 +0200

    Update testrespo.java
    add new print

--- exc2/src/exc2/testrespo.java
+++ exc2/src/exc2/testrespo.java
@@ -3,5 +3,7 @@ package exc2;
 public class testrespo {
 	public static void main(String[] args) {
 				System.out.println("Hello world2021");
+				System.out.println("Hello world- Sem B");
+		
 	}
 }
